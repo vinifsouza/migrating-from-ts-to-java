bc0b8529f8c9ec73786611532f02b6172d97a574
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2025-01-18 18:10:52 -0300

    feat(api): add entities
    Signed-off-by: Vinícius Souza <vinicius.fernando.souza@gmail.com>

---
+++ api/src/main/java/com/vinifsouza/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.vinifsouza.api.domain.address;
+
+import com.vinifsouza.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name =  "event_id")
+    private Event event;
+}
---
+++ api/src/main/java/com/vinifsouza/api/domain/event/Event.java
@@ -0,0 +1,32 @@
+package com.vinifsouza.api.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+import java.util.Date;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String imageUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+}
